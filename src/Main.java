c21cf290a35d658e2d79996d14222ab7b5bc1c0d
SarahGG <guarino.sgg@gmail.com>
2017-08-29 21:31:04 -0400

    adding Main class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main class for Sarah's daily babysitting wage calculator.
+ *
+ * @author Sarah Guarino
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
