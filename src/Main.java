836ec1f6f9e05c318099886e8bc3b422da18fe39
SarahGG <guarino.sgg@gmail.com>
2017-08-30 00:10:54 -0400

    main checks user input for correct range on whole numbers

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,9 @@ public class Main {
         System.out.print("What time did you start?\n>: ");
         userInputTime = scnr.nextLine();
 
-        while(!(Validator.isWholeNumber(userInputTime))) {
-            System.out.print("Please enter a whole number only.\n>: ");
+        while(!(Validator.isWholeNumber(userInputTime))
+                || !(Validator.isWithinTimeRange(Integer.parseInt(userInputTime)))) {
+            System.out.print("Please enter a whole number between 1 and 12 only.\n>: ");
             userInputTime = scnr.nextLine();
         }
     }
